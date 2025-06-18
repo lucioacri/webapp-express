24ae520145dc879b5a9de5d124a4211eec2dd279
lucioacri <lucioacri@gmail.com>
2025-06-18 12:44:59 +0200

    add cors

--- index.js
+++ index.js
@@ -3,15 +3,21 @@
 const express = require("express");
 const notFound = require("./middlewares/notFound");
 const movieRouter = require("./routers/movieRouter");
+const cors = require("cors");
 
 // CONFIG
 const app = express();
 const port = 3000;
+const corsConfig = {
+  origin: "http://localhost:5173",
+};
 
 // MIDDLEWARES
 
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cors(corsConfig));
+
 // ROUTES
 
 app.use("/movies", movieRouter);
